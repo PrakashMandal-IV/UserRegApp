82d99e19ba080e8050da0b2d8438d7df414daa01
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-06-06 13:43:52 +0530

    Add : File UploadFunction

--- UserWebFormApp/Images.aspx
+++ UserWebFormApp/Images.aspx
@@ -6,11 +6,10 @@
          <h1 style="font-size:20px;font-family:Roboto; color:rgb(60, 60, 218); text-align: center; height: 91px; margin-bottom:10pt">Image Section</h1>
         <div class="ImageSelection" style="display:flex; flex-direction:column; justify-items:center; border:1px dashed black; width:200pt; height:200pt; border-radius:10px; object-fit:contain;justify-items:center; align-items:center">
             <p style="text-align:center; margin-top:30%">Choose Image</p> 
-            <asp:FileUpload ID="FileUpload1" runat="server" style="margin-left:30pt;"/>
-            
+            <asp:FileUpload ID="ImageSelector" runat="server" style="margin-left:30pt;"/>      
         </div>
-        <asp:Label ID="msg" runat="server" style="padding:20pt" Text="Label"></asp:Label>
-        <asp:Button ID="Button1" runat="server" style="border: none ; border-radius:5px; padding:5pt; background-image:linear-gradient(to bottom, rgb(133, 133, 190), rgb(96, 96, 255)); color: white; width:100pt" BorderStyle="None" Height="41px" Text="Save" />
+        <asp:Label ID="msg" runat="server" style="padding:20pt; color:red;" Text=""></asp:Label>
+        <asp:Button ID="Button1" runat="server" style="border: none ; border-radius:5px; padding:5pt; background-image:linear-gradient(to bottom, rgb(133, 133, 190), rgb(96, 96, 255)); color: white; width:100pt" BorderStyle="None" Height="41px" Text="Save" OnClick="Upload_Click" />
     </div>
    
 </asp:Content>
--- UserWebFormApp/Images.aspx.cs
+++ UserWebFormApp/Images.aspx.cs
@@ -3,15 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.IO;
 using System.Web.UI.WebControls;
 
 namespace UserWebFormApp
 {
     public partial class About : Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Upload_Click(object sender, EventArgs e)
+        {
+            UploadImage();
+
+            string date = DateTime.Now.ToString("yyyy-mm-dd");
+            string filepath = Server.MapPath("/Image/" + date + "/Original/");
+            Directory.CreateDirectory(filepath);
+        }
+
+
+        protected void UploadImage()
+        {
+            //Check if the file is selected or not 
+            if (ImageSelector.HasFile)
+            {
+                string fileExtension = Path.GetExtension(ImageSelector.FileName).ToLower();
+                string date = DateTime.Now.ToString("yyyy-mm-dd");
+                //Check the file type
+                if (fileExtension == ".jpg" || fileExtension == ".png" || fileExtension == ".jpeg")
+                {
+                    string filepath = Server.MapPath("/Image/" + date + "/Original/");
+                    //check if directory exist or not
+                    if(!Directory.Exists(filepath))
+                    {
+                        Directory.CreateDirectory(filepath);
+                    }
+
+                    ImageSelector.SaveAs(filepath + "NEW FILE NAME");
+                }
+                else msg.Text = "File must be png/jpg/jpeg";
+            }
+            else msg.Text = "Please Select a file";
         }
     }
 }
